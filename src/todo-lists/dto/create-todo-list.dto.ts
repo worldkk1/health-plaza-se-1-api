9344c644a3d1cb609c12649ae171a61b5ca5a716
Krittamate Khamkom <worldkk@Its-my-MacBook-Air.local>
2024-06-22 18:13:14 +0700

    ✨ set content not empty

--- src/todo-lists/dto/create-todo-list.dto.ts
+++ src/todo-lists/dto/create-todo-list.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTodoListDto {
   @IsString()
+  @IsNotEmpty()
   content: string;
 }
--- src/todo-lists/dto/update-todo-list.dto.ts
+++ src/todo-lists/dto/update-todo-list.dto.ts
@@ -1,7 +1,8 @@
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateTodoListDto {
   @IsString()
+  @IsNotEmpty()
   @IsOptional()
   content?: string;
 
